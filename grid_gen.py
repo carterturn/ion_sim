8e5976f450f53c133d87e4769ca4634644023a9a
Carter Turnbaugh <carterturn@tutanota.de>
2017-07-29 19:22:26 -0700

    New grid size (why am I tracking this?)

--- grid_gen.py
+++ grid_gen.py
@@ -1,3 +1,3 @@
-for i in xrange(0, 32):
-    for j in xrange(0, 32):
-        print str(i*16+8) + ".0, " + str(j*16+8) + ".0, 0.0, 0.0, 1.602E-19, 1.672E-27"
+for i in xrange(0, 45):
+    for j in xrange(0, 46):
+        print str(i*10+8) + ".0, " + str(j*10+8) + ".0, 0.0, 0.0, 1.602E-19, 1.672E-27"
