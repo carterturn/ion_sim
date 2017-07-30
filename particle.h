319ac67c8bcee75cdf39ef2d1732e830d36e1d8f
Carter Turnbaugh <carterturn@tutanota.de>
2017-07-29 19:22:36 -0700

    Formatting?

--- particle.h
+++ particle.h
@@ -3,10 +3,10 @@
 #define K 8.987E9
 
 struct particle{
-	double x;
-	double y;
-	double vx;
-	double vy;
-	double q;
-	double m;
+		double x;
+		double y;
+		double vx;
+		double vy;
+		double q;
+		double m;
 };
